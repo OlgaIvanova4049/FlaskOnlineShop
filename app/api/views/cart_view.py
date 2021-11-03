fe7628cabd51ef71e1cd54fd4579fd3339102449
filipp.zotov <filipp.zotov@intexsoft.by>
2021-11-03 18:26:33 +0300

    FOS-8: Changed add product to cart query logic.

--- app/api/views/cart_view.py
+++ app/api/views/cart_view.py
@@ -1,5 +1,5 @@
 import http
-from functools import wraps
+import functools
 
 from flask import Blueprint, request, jsonify
 from jose import jwt
@@ -7,7 +7,6 @@ from jose import jwt
 from app.core.settings import settings
 from app.orm.repository import product_repository, cart_repository, cart_item_repository
 from app.orm.schemas.request.cart.cart import CartSchema
-from app.orm.schemas.request.cart.cart_item import CartItemRequestSchema
 from app.orm.schemas.request.product.product import ProductCartSchema
 from app.orm.schemas.response.cart.cart import CartResponseSchema
 from app.orm.schemas.response.cart.cart_item import CartItemResponseSchema
@@ -17,39 +16,23 @@ cart_blueprint = Blueprint('cart_blueprint', __name__, url_prefix="/cart")
 
 def cart_decorator(f):
     """Get cart_uid from JWT"""
-    @wraps(f)
+    @functools.wraps(f)
     def wrapped(*args, **kwargs):
         auth_header = request.headers.get('Authorization')
-        if auth_header:
-            auth_token = auth_header.split(" ")[1]
+        auth_token = auth_header.removeprefix("Bearer ") if auth_header else ""
         cart_uid = jwt.decode(auth_token, key=settings.secret_key)["uid"]
         return f(cart_uid, *args, **kwargs)
 
     return wrapped
 
-    # TODO: добавить документацию, functools.wraps, исключение
-
 
 @cart_blueprint.route("/add", methods=['POST'], endpoint="add_product")
 @cart_decorator
 def add_product_to_cart(cart_uid):
-    cart = cart_repository.find_by_uid(cart_uid)
-    if not cart:
-        cart = cart_repository.create(CartSchema(uid=cart_uid))
+    cart_schema = CartSchema(uid=cart_uid)
     product_schema = ProductCartSchema.parse_obj(request.json)
-    product = product_repository.find(product_schema.id)
-    product_in_cart = cart_item_repository.product_in_cart(cart.id, product_schema.id)
-    if product_in_cart:
-        cart_item_repository.update_quantity(product_in_cart.id, product_schema.quantity)
-    else:
-        cart_item_repository.create(CartItemRequestSchema(cart_id=cart.id,
-                                                          product_id=product.id,
-                                                          price=product.price,
-                                                          quantity=product_schema.quantity))
-    cart = cart_repository.find_by_uid(cart_uid)
-    product_repository.update_quantity(product_schema.id, product_schema.quantity)
-    cart_repository.update_price(cart.count_total_price(), cart_uid)
-    # TODO: изменить количество продукта+, сделать метод в модели для подсчеты total price+, композитный индекс+
+    cart = cart_repository.add_product_to_cart(cart_schema, product_schema)
+
     return CartResponseSchema.from_orm(cart).json(), http.HTTPStatus.CREATED
 
 
--- app/orm/repository/carts/cart_repository.py
+++ app/orm/repository/carts/cart_repository.py
@@ -1,5 +1,10 @@
 from app.orm.models.cart.cart_model import CartModel
+from app.orm.models.product.product_model import ProductModel
+from app.orm.repository import cart_item_repository, product_repository
 from app.orm.repository.base import BaseRepository, session_scope
+from app.orm.schemas.request.cart.cart import CartSchema
+from app.orm.schemas.request.cart.cart_item import CartItemRequestSchema
+from app.orm.schemas.request.product.product import ProductCartSchema
 
 
 class CartRepository(BaseRepository):
@@ -17,5 +22,25 @@ class CartRepository(BaseRepository):
             session.commit()
             return cart
 
+    def add_product_to_cart(self, cart_schema: CartSchema, product_schema: ProductCartSchema):
+        cart = self.find_by_uid(cart_schema.uid)
+        if not cart:
+            cart = self.create(cart_schema)
 
+        with session_scope() as session:
+            product = session.query(ProductModel).get(id)
+            cart_item = session.query(self.model).filter_by(product_id=product_schema.id, cart_id=cart.id).first()
+            if cart_item:
+                cart_item.quantity += product_schema.quantity
+            else:
+                cart_item_repository.create(
+                    CartItemRequestSchema(
+                        cart_id=cart.id,
+                        product_id=product.id,
+                        price=product.price,
+                        quantity=product_schema.quantity
+                    )
+                )
+            product.quantity -= product_schema.quantity
 
+        return cart
