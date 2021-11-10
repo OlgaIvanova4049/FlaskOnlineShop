e204efca78dd898613222b0f6dcb8c1d5590e2a7
Olga Ivanova <olga.ivanova@intexsoft.by>
2021-11-10 13:37:49 +0300

    Corrected Subject line in message

--- app/worker/tasks/email.py
+++ app/worker/tasks/email.py
@@ -5,9 +5,7 @@ import smtplib, ssl
 
 @celery.task(name='send_admin_email', queue='email.send')
 def send_admin_email(order_id, admin_email):
-    message = f"""\
-    Subject: Order successfully created
-
+    message = f"""Subject: Order successfully created\n\n
     The order {order_id} was created!"""
 
     context = ssl.create_default_context()
@@ -17,9 +15,7 @@ def send_admin_email(order_id, admin_email):
 
 @celery.task(name='send_user_email', queue='email.send')
 def send_user_email(order_id, user_email):
-    message = f"""\
-    Subject: Order successfully created
-
+    message = f"""Subject: Order successfully created\n\n
     We received your order {order_id}. Our operator will contact you soon.
     
     Thank you!"""
